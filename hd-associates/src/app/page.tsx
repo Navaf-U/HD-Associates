87d01ca479f2171f60ebcbbc52a0573366cb9822
navaf <navafsoft@gmail.com>
2025-04-28 11:26:39 +0530

    add Navbar component with navigation links and create layout structure

---
+++ hd-associates/src/app/page.tsx
@@ -0,0 +1,10 @@
+import WhyUs from '@/components/WhyUs'
+import Hero from '../../src/components/Hero'
+export default function Home() {
+  return (
+    <main className="pt-20">
+      <Hero />
+      <WhyUs/>
+    </main>
+  )
+}
---
+++ hd-associates/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+export default function Navbar() {
+    return (
+        <nav className="fixed flex h-24 items-center top-0 left-0 w-full bg-white text-[#000000c5] shadow-md z-50">
+            <Image width={100} height={100} src={'/assets/logo.png'} className="text-xl font-bold w-18 h-18 ms-10" alt='logo' />
+            <div className="max-w-7xl mx-auto px-4 py-3 flex justify-center items-center">
+                <div className="flex gap-6">
+                    <Link href="/" className="hover:text-blue-500">Home</Link>
+                    <Link href="/about" className="hover:text-blue-500">About Us</Link>
+                    <Link href="/commitment" className="hover:text-blue-500">Our Commitment</Link>
+                    <Link href="/team-members" className="hover:text-blue-500">Team Members</Link>
+                    <Link href="/service" className="hover:text-blue-500">Our Services</Link>
+                    <Link href="/client" className="hover:text-blue-500">Our Clients</Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
